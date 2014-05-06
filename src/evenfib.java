d646055aab031fda450699f6395d3fd53a25886c
caren <cchang@tagged.com>
2014-05-05 21:34:18 -0700

    first commit with two problems solved

---
+++ src/evenfib.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+
+public class evenfib {
+
+	/**
+	 * By considering the terms in the Fibonacci sequence whose values do not exceed four million
+	 * find the sum of the even-valued terms
+	 */
+	public static void main(String[] args) {
+		
+		int firstNum = 1;
+		int secondNum = 2;
+		int oldFirstNum = 0;
+		int sumOfFibs = 0;
+		
+		while (secondNum < 4000000) {
+			if (secondNum % 2 == 0) {
+				sumOfFibs = sumOfFibs + secondNum;
+			}
+			
+			oldFirstNum = firstNum;
+			firstNum = secondNum;
+			secondNum = oldFirstNum + secondNum;
+		}
+		
+		System.out.println(sumOfFibs);
+
+	}
+
+}
