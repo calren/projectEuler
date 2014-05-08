8ecb713b1a1b2034043da218f9d9dbebeb04778a
Caren Chang <cchang@tagged.com>
2014-05-07 17:20:15 -0700

    starting p41

---
+++ src/p40.java
@@ -0,0 +1,18 @@
+
+public class p40 {
+    
+    /**
+     * 
+     */
+    public static void main(String[] args) { 
+        
+    }
+
+    public String[] getBigNumber(int digits, int endNumber) {
+        String s = "";
+        
+        while (s.length() < digits) {
+            s = 
+        }
+    }
+}
