22d3fff7383145da435c0c854f6f9e6609a28610
caren <cchang@tagged.com>
2014-05-07 21:46:48 -0700

    finishing p40

--- src/p40.java
+++ src/p40.java
@@ -5,23 +5,24 @@ public class p40 {
      * 
      */
     public static void main(String[] args) { 
-        System.out.println(getBigNumber(1) * getBigNumber(10) * getBigNumber(100) * getBigNumber(1000) 
-                * getBigNumber(10000));
-        
-//         * getBigNumber(100000) * getBigNumber(1000000)
+    	
+    	System.out.println(getBigNumber(1));
+    	System.out.println(getBigNumber(10));
+    	System.out.println(getBigNumber(100));
+    	System.out.println(getBigNumber(1000));
+    	System.out.println(getBigNumber(10000));
+    	System.out.println(getBigNumber(100000));
     }
 
-    public static int getBigNumber(int digits) {
+    public static char getBigNumber(int digits) {
         String s = "";
-        int startNumber = 1;
+        int startNumber = 0;
         
-        while (s.length() < digits) {
+        while (s.length() <= digits) {
             s = s + (startNumber + 1);
             startNumber++;
         }
         
-        System.out.println(startNumber);
-        
-        return (int) s.charAt(s.length()-1);
+        return s.charAt(digits-1);
     }
 }
